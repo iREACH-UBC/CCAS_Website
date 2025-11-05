267609b586744a740e7eb069959525609ea6db27
Dignoes Ricart <hugo.dignoes@ubc.ca>
2025-11-04 20:20:56 -0800

    Removed MOD 628 from sensor list (double counted VPL)

--- js/data.js
+++ js/data.js
@@ -14,7 +14,7 @@ export async function getSensorData () {
 
   _cache = await res.json();
   
-  const blacklist = ["MOD-00616"];
+  const blacklist = ["MOD-00616", "MOD-00628"];
   _cache.sensors = _cache.sensors.filter(s => !blacklist.includes(s.id));
 
   return _cache;
